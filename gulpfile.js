ee352ccdc902e1a400cbbb3aa11aa4742126a524
test <flantan233@gmail.com>
2018-07-13 00:01:42 +0800

    udpate

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp')
 const del = require('del')
 const less = require('gulp-less')
+const watch = require('gulp-watch')
 
 gulp.task('clean', function(){
   return del(['dist'])
@@ -15,7 +16,18 @@ gulp.task('less', ['clean'], function(){
 gulp.task('move', ['clean'], function(){
   gulp.src('src/**/*.html')
     .pipe(gulp.dest('dist'));
+  gulp.src('src/**/*.css')
+    .pipe(gulp.dest('dist'));
+  gulp.src('src/**/*.js')
+    .pipe(gulp.dest('dist'));
+  gulp.src('src/**/images/*')
+    .pipe(gulp.dest('dist'));
+  console.log(`build time: ${new Date()}`)
 })
+
+gulp.task('watch', function(){
+  gulp.watch('src/**/*', ['clean', 'less', 'move']);
+});
 
 gulp.task('default', ['clean', 'less', 'move'], function() {
 });
